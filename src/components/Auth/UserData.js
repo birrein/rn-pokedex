72505674d2bae5340f0ef4afabaf3a94384eb815
Manuel Marín <birrein@gmail.com>
2022-01-14 18:55:21 -0300

    Basic login & validation

---
+++ src/components/Auth/UserData.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const UserData = () => {
+  return (
+    <View>
+      <Text>User Data...</Text>
+    </View>
+  );
+};
+
+export default UserData;
---
+++ src/utils/userDB.js
@@ -0,0 +1,11 @@
+export const user = {
+  username: "birrein",
+  password: "123456",
+};
+
+export const userDetails = {
+  username: "birrein",
+  firstName: "Manuel",
+  lastName: "Marín",
+  email: "birrein@gmail.com",
+};
